a956a27fdfdd7614c77f949c97fdd3d620210aa8
Sajjad <rabiee.614@gmail.com>
2020-11-07 21:45:05 +0330

    build entity of User

--- src/main/java/Service/Entities/User.java
+++ src/main/java/Service/Entities/User.java
@@ -1,4 +1,18 @@
 package Service.Entities;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column
+    private String name;
+    @Column
+    private Gender gender;
+    @OneToMany(mappedBy = "user")
+    private List<Ticket> tickets = new ArrayList<>();
 }
