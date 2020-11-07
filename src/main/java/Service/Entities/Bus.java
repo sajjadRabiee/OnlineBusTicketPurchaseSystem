3f501e388d943523a2cdfa677c6ea72b3fbd108e
Sajjad <rabiee.614@gmail.com>
2020-11-07 22:58:23 +0330

    build entity of Bus

--- src/main/java/Service/Entities/Bus.java
+++ src/main/java/Service/Entities/Bus.java
@@ -1,4 +1,16 @@
 package Service.Entities;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Bus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @ManyToMany(mappedBy = "buses")
+    private List<Travel> travels = new ArrayList<>();
+
 }
