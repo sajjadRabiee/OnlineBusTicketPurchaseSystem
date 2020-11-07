5d54bce768ae4a144e4123e9c56d362f43e8d85f
Sajjad <rabiee.614@gmail.com>
2020-11-07 23:00:18 +0330

    build getter and setter of Travel

--- src/main/java/Service/Entities/Travel.java
+++ src/main/java/Service/Entities/Travel.java
@@ -27,4 +27,44 @@ public class Travel {
             inverseJoinColumns = {@JoinColumn(name = "bus_id")}
     )
     private List<Bus> buses = new ArrayList<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBeginning() {
+        return beginning;
+    }
+
+    public void setBeginning(String beginning) {
+        this.beginning = beginning;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public Date getTimeOfMovement() {
+        return timeOfMovement;
+    }
+
+    public void setTimeOfMovement(Date timeOfMovement) {
+        this.timeOfMovement = timeOfMovement;
+    }
+
+    public List<Bus> getBuses() {
+        return buses;
+    }
+
+    public void setBuses(List<Bus> buses) {
+        this.buses = buses;
+    }
 }
