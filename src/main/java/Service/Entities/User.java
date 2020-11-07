e968dffd91d4ae3299cb23836e129d4e4c30fd42
Sajjad <rabiee.614@gmail.com>
2020-11-07 23:00:04 +0330

    build getter and setter of User

--- src/main/java/Service/Entities/User.java
+++ src/main/java/Service/Entities/User.java
@@ -9,10 +9,45 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
     @Column
     private String name;
+
     @Column
     private Gender gender;
+
     @OneToMany(mappedBy = "user")
     private List<Ticket> tickets = new ArrayList<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
 }
