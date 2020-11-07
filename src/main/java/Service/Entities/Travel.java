e5a123941e9b68239d1ee4e1141c854a7f912ed6
Sajjad <rabiee.614@gmail.com>
2020-11-07 22:58:52 +0330

    build entity of Travel

--- src/main/java/Service/Entities/Travel.java
+++ src/main/java/Service/Entities/Travel.java
@@ -1,4 +1,30 @@
 package Service.Entities;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
 public class Travel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column
+    private String beginning;
+
+    @Column
+    private String destination;
+
+    @Column
+    private Date timeOfMovement;
+
+    @ManyToMany
+    @JoinTable(
+            name = "travels_buses",
+            joinColumns = {@JoinColumn(name = "travel_id")},
+            inverseJoinColumns = {@JoinColumn(name = "bus_id")}
+    )
+    private List<Bus> buses = new ArrayList<>();
 }
