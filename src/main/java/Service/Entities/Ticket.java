ad3de5597d916553eeabd42fe4bd485f807820a2
Sajjad <rabiee.614@gmail.com>
2020-11-07 22:57:13 +0330

    build entity of Ticket

--- src/main/java/Service/Entities/Ticket.java
+++ src/main/java/Service/Entities/Ticket.java
@@ -1,4 +1,35 @@
 package Service.Entities;
 
 public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private Gender gender;
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "ticket_number_generator")
+    @SequenceGenerator(
+            name="ticket_number_generator",
+            sequenceName = "ticket_number_seq")
+    @Column(name = "ticket_number")
+    private long ticketNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_user")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_travel")
+    private Travel travel;
+
+    @Column
+    @Generated(GenerationTime.ALWAYS)
+    private Date ticketIssuanceTime;
+
 }
