d2b88cffd00616660744b1431c954121836ce58b
Sajjad <rabiee.614@gmail.com>
2020-11-07 23:08:00 +0330

    create base entity dao for CRUD entities

---
+++ src/main/java/Repository/DAOEntities/BaseEntityDAO.java
@@ -0,0 +1,124 @@
+package Repository.DAOEntities;
+
+import Repository.EntityManagerFactory.EMF;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+import java.util.Optional;
+
+public abstract class BaseEntityDAO<Entity, ID extends Number> {
+    protected Class<Entity> entityClass;
+
+    protected abstract void setEntityClass();
+
+    protected String columnName;
+
+    protected abstract void setColumnName(String columnName);
+
+    //find
+    //  byID
+    public Optional<Entity> findByID(ID id) {
+        EntityManager em = EMF.getEntityManagerFactory().createEntityManager();
+        Entity entity;
+        try {
+            entity = em.find(entityClass, id);
+        } catch (Exception e) {
+            entity = null;
+            System.out.println(e);
+        } finally {
+            em.close();
+        }
+        return entity != null ? Optional.of(entity) : Optional.ofNullable(null);
+    }
+
+    //  byName
+    public Optional<Entity> findByName(String name) {
+        Entity entity;
+        EntityManager em = EMF.getEntityManagerFactory().createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        try {
+            transaction.begin();
+            CriteriaQuery<Entity> criteriaQuery = criteriaBuilder.createQuery(entityClass);
+            Root<Entity> root = criteriaQuery.from(entityClass);
+            criteriaQuery.where(criteriaBuilder.like(root.get(columnName), name));
+            TypedQuery<Entity> entityTypedQuery = em.createQuery(criteriaQuery);
+            entity = entityTypedQuery.getSingleResult();
+            transaction.commit();
+        } catch (Exception e) {
+            System.out.println(e);
+            transaction.rollback();
+            entity = null;
+        } finally {
+            em.close();
+        }
+        return entity != null ? Optional.of(entity) : Optional.ofNullable(null);
+    }
+
+    // all
+    public List<Entity> findAll(){
+        EntityManager em = EMF.getEntityManagerFactory().createEntityManager();
+        TypedQuery<Entity> query = em.createQuery("select e from "+entityClass.getName()+" e",entityClass);
+        List<Entity> objects = query.getResultList();
+        return objects;
+    }
+    //add
+    public boolean add(Entity entity) {
+        EntityManager em = EMF.getEntityManagerFactory().createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+            em.persist(entity);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            transaction.rollback();
+            System.out.println(e);
+            return false;
+        } finally {
+            em.close();
+        }
+    }
+
+    //update
+    public boolean update(Entity entity) {
+        EntityManager em = EMF.getEntityManagerFactory().createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+            em.merge(entity);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            transaction.rollback();
+            System.out.println(e);
+            return false;
+        } finally {
+            em.close();
+        }
+    }
+
+    //delete
+    public boolean delete(Entity entity) {
+        EntityManager em = EMF.getEntityManagerFactory().createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+            em.remove(entity);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            transaction.rollback();
+            System.out.println(e);
+            return false;
+        } finally {
+            em.close();
+        }
+    }
+}
+
