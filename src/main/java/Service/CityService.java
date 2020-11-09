09f6003fca26decd666b8986288c723a77d54d71
Sajjad <rabiee.614@gmail.com>
2020-11-09 19:09:25 +0330

    create and implements method for city service

---
+++ src/main/java/Service/CityService.java
@@ -0,0 +1,49 @@
+package Service;
+
+import Repository.DAOEntities.CityDAO;
+import Service.Entities.City;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CityService implements BaseEntityService<City>{
+    CityDAO cityDAO = new CityDAO();
+
+    public CityService(){
+        cityDAO.setEntityClass();
+    }
+
+    @Override
+    public Optional<City> findById(long id) {
+        Optional<City> byID = cityDAO.findByID(id);
+        return byID;
+    }
+
+    @Override
+    public Optional<City> findByName(String name, String column) {
+        cityDAO.setColumnName(column);
+        Optional<City> byName = cityDAO.findByName(name);
+        return byName;
+    }
+
+    @Override
+    public List<City> findAll() {
+        List<City> all = cityDAO.findAll();
+        return all;
+    }
+
+    @Override
+    public boolean add(City city) {
+        return cityDAO.add(city);
+    }
+
+    @Override
+    public boolean delete(City city) {
+        return cityDAO.delete(city);
+    }
+
+    @Override
+    public boolean update(City city) {
+        return cityDAO.update(city);
+    }
+}
