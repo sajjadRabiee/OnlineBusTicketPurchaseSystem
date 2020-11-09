d15390c744a39142b040279c4a98bb9bedf0a9c4
Sajjad <rabiee.614@gmail.com>
2020-11-09 18:42:12 +0330

    design methods implementations of Ticket Service

--- src/main/java/Service/TicketService.java
+++ src/main/java/Service/TicketService.java
@@ -1,38 +1,50 @@
 package Service;
 
+import Repository.DAOEntities.TicketDAO;
 import Service.Entities.Ticket;
 
 import java.util.List;
 import java.util.Optional;
 
 public class TicketService implements BaseEntityService<Ticket> {
-    @Override
-    public Optional<Ticket> findById() {
-        return Optional.empty();
+
+    TicketDAO ticketDAO = new TicketDAO();
+
+    public TicketService(){
+        ticketDAO.setEntityClass();
     }
 
     @Override
-    public Optional<Ticket> findByName() {
-        return Optional.empty();
+    public Optional<Ticket> findById(long id) {
+        Optional<Ticket> byID = ticketDAO.findByID(id);
+        return byID;
+    }
+
+    @Override
+    public Optional<Ticket> findByName(String name , String column) {
+        ticketDAO.setColumnName(column);
+        Optional<Ticket> byName = ticketDAO.findByName(name);
+        return byName;
     }
 
     @Override
     public List<Ticket> findAll() {
-        return null;
+        List<Ticket> all = ticketDAO.findAll();
+        return all;
     }
 
     @Override
-    public boolean add() {
-        return false;
+    public boolean add(Ticket ticket) {
+        return ticketDAO.add(ticket);
     }
 
     @Override
-    public boolean delete() {
-        return false;
+    public boolean delete(Ticket ticket) {
+        return ticketDAO.delete(ticket);
     }
 
     @Override
-    public boolean update() {
-        return false;
+    public boolean update(Ticket ticket) {
+        return ticketDAO.update(ticket);
     }
 }
