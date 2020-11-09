10c11cf9e96598d69f71822389c21ed0e8cb3432
Sajjad <rabiee.614@gmail.com>
2020-11-09 18:07:36 +0330

    add something to user entity

--- src/main/java/Service/Entities/User.java
+++ src/main/java/Service/Entities/User.java
@@ -14,6 +14,12 @@ public class User {
     private String name;
 
     @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
     private Gender gender;
 
     @OneToMany(mappedBy = "user")
@@ -33,6 +39,22 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public Gender getGender() {
