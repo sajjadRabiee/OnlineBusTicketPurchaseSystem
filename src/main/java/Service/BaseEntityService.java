0abecdbe06bcbe6314c87bf169f21feef265a16d
Sajjad <rabiee.614@gmail.com>
2020-11-09 18:34:39 +0330

    change interface for better performance

--- src/main/java/Service/BaseEntityService.java
+++ src/main/java/Service/BaseEntityService.java
@@ -4,10 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BaseEntityService<Entity> {
-    Optional<Entity> findById();
-    Optional<Entity> findByName();
+    Optional<Entity> findById(long id);
+    Optional<Entity> findByName(String name , String column);
     List<Entity> findAll();
-    boolean add();
-    boolean delete();
-    boolean update();
+    boolean add(Entity entity);
+    boolean delete(Entity entity);
+    boolean update(Entity entity);
 }
