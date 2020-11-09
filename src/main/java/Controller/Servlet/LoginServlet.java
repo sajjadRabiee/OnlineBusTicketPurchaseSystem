022bee14f1aaff5bc71632db195ead85c069cea3
Sajjad <rabiee.614@gmail.com>
2020-11-09 23:13:28 +0330

    build login servlet

--- src/main/java/Controller/Servlet/LoginServlet.java
+++ src/main/java/Controller/Servlet/LoginServlet.java
@@ -1,14 +1,32 @@
 package Controller.Servlet;
 
+import Service.Entities.User;
+import Service.UserService;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Optional;
 
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getParameter("username");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        UserService userService = new UserService();
+        Optional<User> oUser = userService.loginUser(username, password);
+        if(oUser.isPresent()){
+            User user = oUser.get();
+            HttpSession httpSession = req.getSession();
+            httpSession.setAttribute("online_user",user);
+            resp.sendRedirect("/dashboard");
+        }else{
+            resp.sendRedirect("login.html");
+        }
     }
 }
