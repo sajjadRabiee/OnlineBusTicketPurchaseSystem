9c52308f560c87ac77952a5b113e82496151e9ab
Sajjad <rabiee.614@gmail.com>
2020-11-09 23:06:26 +0330

    build security filter for user of site

---
+++ src/main/java/Controller/Filter/SecurityFilter.java
@@ -0,0 +1,33 @@
+package Controller.Filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/dashboard")
+public class SecurityFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpReq = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResp = (HttpServletResponse) servletResponse;
+        HttpSession session = httpReq.getSession();
+        if(session.getAttribute("online_user")==null){
+            filterChain.doFilter(servletRequest,servletResponse);
+        }else{
+            httpResp.sendRedirect("login.html");
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
