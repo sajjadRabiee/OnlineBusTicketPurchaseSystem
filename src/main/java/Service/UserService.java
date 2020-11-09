98c192833b6491825f41cd90f3ae01de69dae866
Sajjad <rabiee.614@gmail.com>
2020-11-09 18:35:52 +0330

    design methods implementations of User Service

--- src/main/java/Service/UserService.java
+++ src/main/java/Service/UserService.java
@@ -1,38 +1,50 @@
 package Service;
 
+import Repository.DAOEntities.UserDAO;
 import Service.Entities.User;
 
 import java.util.List;
 import java.util.Optional;
 
 public class UserService implements BaseEntityService<User> {
-    @Override
-    public Optional<User> findById() {
-        return Optional.empty();
+
+    UserDAO userDAO = new UserDAO();
+
+    public UserService(){
+        userDAO.setEntityClass();
     }
 
     @Override
-    public Optional<User> findByName() {
-        return Optional.empty();
+    public Optional<User> findById(long id) {
+        Optional<User> byID = userDAO.findByID(id);
+        return byID;
+    }
+
+    @Override
+    public Optional<User> findByName(String name , String column) {
+        userDAO.setColumnName(column);
+        Optional<User> byName = userDAO.findByName(name);
+        return byName;
     }
 
     @Override
     public List<User> findAll() {
-        return null;
+        List<User> all = userDAO.findAll();
+        return all;
     }
 
     @Override
-    public boolean add() {
-        return false;
+    public boolean add(User user) {
+        return userDAO.add(user);
     }
 
     @Override
-    public boolean delete() {
-        return false;
+    public boolean delete(User user) {
+        return userDAO.delete(user);
     }
 
     @Override
-    public boolean update() {
-        return false;
+    public boolean update(User user) {
+        return userDAO.update(user);
     }
 }
