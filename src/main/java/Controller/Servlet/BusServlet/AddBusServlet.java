3b453e2412acaac07dfdc8338957edb6b3967967
Sajjad <rabiee.614@gmail.com>
2020-11-10 13:28:00 +0330

    build bus servlet

---
+++ src/main/java/Controller/Servlet/BusServlet/AddBusServlet.java
@@ -0,0 +1,25 @@
+package Controller.Servlet.BusServlet;
+
+import Service.BusService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add-bus")
+public class AddBusServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int busNumber = Integer.parseInt(req.getParameter("bus-number"));
+        int valency = Integer.parseInt(req.getParameter("valency"));
+        BusService busService = new BusService();
+        if(busService.addBus(busNumber,valency)){
+            resp.sendRedirect("/dashboard/admin/add-bus.html?true");
+        }else{
+            resp.sendRedirect("/dashboard/admin/add-bus.html?true");
+        }
+    }
+}
