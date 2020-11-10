d98d25b2638806f3d3d5e636f1fff5ee9a5b9608
Sajjad <rabiee.614@gmail.com>
2020-11-10 11:59:12 +0330

    build login filter for limit access to somewhere of site

---
+++ src/main/java/Controller/Filter/LoginFilter.java
@@ -0,0 +1,43 @@
+package Controller.Filter;
+
+import Service.Entities.User;
+import Service.UserService;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Optional;
+
+@WebFilter("/login")
+public class LoginFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        UserService userService = new UserService();
+        Optional<User> oUser = userService.loginUser(username, password);
+        if(oUser.isPresent()){
+            User user = oUser.get();
+            HttpSession httpSession = req.getSession();
+            httpSession.setAttribute("online_user",user);
+            resp.sendRedirect("/login");
+        }else{
+            resp.sendRedirect("login.html");
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
